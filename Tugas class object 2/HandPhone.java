17a25e33ea8c7d451609acac287c64ec72d6352c
yassin <yassin.novian_ti24@nusaputra.ac.id>
2026-03-23 18:11:11 +0700

    Add files via upload

---
+++ Tugas class object 2/HandPhone.java
@@ -0,0 +1,31 @@
+public class HandPhone {
+    // Atribut / field
+    String jenis_hp;
+    int tahun_pembuatan;
+
+    // Setter method
+    public void setDataHP(String jenis_hp, int tahun_pembuatan) {
+        this.jenis_hp = jenis_hp;
+        this.tahun_pembuatan = tahun_pembuatan;
+    }
+
+    // Getter untuk jenis_hp
+    public String getJenisHP() {
+        return this.jenis_hp;
+    }
+
+    // Getter untuk tahun_pembuatan
+    public int getTahunPembuatan() {
+        return this.tahun_pembuatan;
+    }
+
+    // Method utama (entry point)
+    public static void main(String[] args) {
+        HandPhone hp = new HandPhone();
+        hp.setDataHP("Samsung", 2023);
+
+        // Menampilkan hasil
+        System.out.println("Jenis HP     : " + hp.getJenisHP());
+        System.out.println("Tahun Buat   : " + hp.getTahunPembuatan());
+    }
+}
